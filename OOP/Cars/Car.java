fbd7846e3dc6c7f03922f02298b2226fa2fa21a4
PratikGarai <pratikgarai0208@gmail.com>
2021-06-28 11:41:10 +0530

    Simple class

---
+++ OOP/Cars/Car.java
@@ -0,0 +1,10 @@
+package Cars;
+
+public class Car {
+    
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+}
---
+++ OOP/Cars_Main.java
@@ -0,0 +1,8 @@
+import Cars.Car;
+
+public class Cars_Main {
+
+    public static void main(String[] args) {
+        Car ob = new Car();
+    }
+}
