64fa2aadb872e97a9599236cb6d8badf5b72564a
PratikGarai <pratikgarai0208@gmail.com>
2021-06-28 13:20:55 +0530

    Added getters and setters

--- OOP/Cars/Car.java
+++ OOP/Cars/Car.java
@@ -7,4 +7,48 @@ public class Car {
     private String model;
     private String engine;
     private String color;
+
+
+    public int getDoors() {
+        return this.doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return this.wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getEngine() {
+        return this.engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    
+
 }
--- OOP/Cars_Main.java
+++ OOP/Cars_Main.java
@@ -3,6 +3,7 @@ import Cars.Car;
 public class Cars_Main {
 
     public static void main(String[] args) {
-        Car ob = new Car();
+        Car porshe = new Car();
+        porshe.setModel("Carrera");
     }
 }
