0a1a68d557752b2141011a81258caca1b0a041d2
PratikGarai <pratikgarai0208@gmail.com>
2021-08-02 07:42:16 +0530

    Inner class 2 driver added

--- Inner Classes/NonStaticNestedClass/GearBoxDriver.java
+++ Inner Classes/NonStaticNestedClass/GearBoxDriver.java
@@ -6,5 +6,16 @@ public class GearBoxDriver {
         GearBox.Gear first = mcLaren.new Gear(1, 1.5);
         GearBox.Gear second = mcLaren.new Gear(2, 2.5);
         GearBox.Gear third = mcLaren.new Gear(3, 3.5);
+
+
+        GearBoxPrivy ferrari = new GearBoxPrivy(6);
+        ferrari.addGear(1, 1.5);
+        ferrari.addGear(2, 3);
+        ferrari.addGear(3, 4.5);
+
+        ferrari.operateClutch(true);
+        ferrari.changeGear(1);
+        ferrari.operateClutch(false);
+        ferrari.changeGear(3); 
     }
 }
